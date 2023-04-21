a7a384017c06ba2cbd97086af4efe6373b63b149
Shashank Tiwari <shashanktiwari3509@gmail.com>
2023-04-21 23:40:40 +0530

    search-bar

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { faShop } from '@fortawesome/free-solid-svg-icons'
+import { faCalendarDay, faShop } from '@fortawesome/free-solid-svg-icons'
 import { faCartShopping } from '@fortawesome/free-solid-svg-icons'
 import { faTruckArrowRight } from '@fortawesome/free-solid-svg-icons'
 import { faPumpMedical } from '@fortawesome/free-solid-svg-icons'
@@ -28,15 +28,41 @@ export const Header = () => {
           </div>
 
           <div className="header-list-items">
-          <FontAwesomeIcon icon={faPumpMedical} />
+            <FontAwesomeIcon icon={faPumpMedical} />
             <span>Health-care</span>
-            
+
+          </div>
+
+          <div className="header-list-items">
+            <FontAwesomeIcon icon={faCookie} />
+            <span>Cookie</span>
+          </div>
+        </div>
+        <h1 className="header-text"> Get anything fron anywhere with Shop-Alley</h1>
+        <p className='header-des'>
+          Redeem coupnes and do unlimited shopinf for your household
+          grocerries and many more
+        </p>
+        <button className="headerBtn">
+          Sign /SignUp
+        </button>
+        <div className="header-search">
+          <div className="header-search-content">
+            <FontAwesomeIcon icon={faShop} className="header-icon" />
+            <input type="text" placeholder="What you want to buy ?"
+              className="header-search-field" />
           </div>
           
-          <div className="header-list-items">
-          <FontAwesomeIcon icon={faCookie} />
-            <span>Cookie</span>
-            </div>
+          <div className="header-search-content">
+            <FontAwesomeIcon icon={faCalendarDay} className="header-icon" />
+            <span className="headerSearchText">Delivery time</span>
+          </div>
+          
+          <div className="header-search-content">
+            <FontAwesomeIcon icon={'calendar-day'} className="header-icon" />
+            <span className="headerSearchText">2days 3days 4days</span>
+          </div>
+
         </div>
       </div>
 
