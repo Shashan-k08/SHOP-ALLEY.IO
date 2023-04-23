aaf93cb481cc613d2c4c52cd8cd2e2e07af7202b
Shashank Tiwari <shashanktiwari3509@gmail.com>
2023-04-23 18:51:32 +0530

    featured shops.

--- client/src/components/FeaturedShops.jsx
+++ client/src/components/FeaturedShops.jsx
@@ -2,7 +2,40 @@ import React from 'react'
 
 const FeaturedShops = () => {
   return (
-    <div>FeaturedShops</div>
+    <div className="fShop">
+      <div className="fShopItem">
+        <img src='https://www.supermarketnews.com/sites/supermarketnews.com/files/styles/article_featured_retina/public/StopShop_Hartford_3.png?itok=sFJwLNO6' alt='' className="fShopImg" />
+        <span className="fShopName"> Stop Stop pvt</span>
+        <span className="fShopCity"> Gurugram</span>
+        <span className="fShopPrice">Buy under $23</span>
+        <div className="fShopRating">
+          <button>8</button>
+          <span>Good</span>
+        </div>
+      </div>
+      
+      <div className="fShopItem">
+        <img src='https://www.bhf.org.uk/-/media/images/shop/shop-charity-shops-shop-front-570x380px.png?rev=eec9cd1141e64cb9a6750eb3d0ee1546' alt='' className="fShopImg" />
+        <span className="fShopName"> Stop Stop pvt</span>
+        <span className="fShopCity"> Gurugram</span>
+        <span className="fShopPrice">Buy under $23</span>
+        <div className="fShopRating">
+          <button>8</button>
+          <span>Good</span>
+        </div>
+      </div>
+      
+      <div className="fShopItem">
+        <img src='https://thumbs.dreamstime.com/b/shop-building-colorful-isolated-white-33822015.jpg' alt='' className="fShopImg" />
+        <span className="fShopName"> Stop Stop pvt</span>
+        <span className="fShopCity"> Gurugram</span>
+        <span className="fShopPrice">Buy under $23</span>
+        <div className="fShopRating">
+          <button>8</button>
+          <span>Good</span>
+        </div>
+      </div>
+    </div>
   )
 }
 
