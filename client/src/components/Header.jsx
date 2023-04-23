6d1b79ea9e3e1982ae490c25509a4957eba39042
Shashank Tiwari <shashanktiwari3509@gmail.com>
2023-04-23 23:52:48 +0530

    search box

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -9,8 +9,10 @@ import { DateRange } from 'react-date-range';
 import { useState } from 'react'
 import 'react-date-range/dist/styles.css';
 import 'react-date-range/dist/theme/default.css';
+import { useNavigate } from "react-router-dom"
 import { format } from "date-fns"
-export const Header = ({type}) => {
+export const Header = ({ type }) => {
+  const navigate = useNavigate();
   const [opendate, setopendate] = useState(false);
   const [date, setdate] = useState([
     {
@@ -35,9 +37,12 @@ export const Header = ({type}) => {
       };
     });
   }
+  const handleSearch = () => {
+    navigate("/shops")
+  }
   return (
     <div className="header">
-      <div className={type==="list" ? "header-content listMode":"header-content"}>
+      <div className={type === "list" ? "header-content listMode" : "header-content"}>
         <div className="header-items">
           <div className="header-list-items">
             <FontAwesomeIcon icon={faShop} />
@@ -65,70 +70,74 @@ export const Header = ({type}) => {
             <span>Cookie</span>
           </div>
         </div>
-        {type !=="list" &&
+        {type !== "list" &&
           <> <h1 className="header-text"> Get anything fron anywhere with Shop-Alley</h1>
-          <p className='header-des'>
-            Redeem coupnes and do unlimited shopinf for your household
-            grocerries and many more
-          </p>
-          <button className="headerBtn">
-            Sign /SignUp
-          </button>
-          <div className="header-search">
-            <div className="header-search-content">
-              <FontAwesomeIcon icon={faShop} className="header-icon" />
-              <input type="text" placeholder="What you want to buy ?"
-                className="header-search-field" />
-            </div>
+            <p className='header-des'>
+              Redeem coupnes and do unlimited shopinf for your household
+              grocerries and many more
+            </p>
+            <button className="headerBtn">
+              Sign /SignUp
+            </button>
+            <div className="header-search">
+              <div className="header-search-content">
+                <FontAwesomeIcon icon={faShop} className="header-icon" />
+                <input type="text" placeholder="What you want to buy ?"
+                  className="header-search-field" />
+              </div>
 
-            <div className="header-search-content">
-              <FontAwesomeIcon icon={faCalendarDay} className="header-icon" />
-              <span onClick={() => setopendate(!opendate)} className="headerSearchText">{`${format(date[0].startDate, "MM/dd/yyyy")} to ${format(date[0].endDate, "MM/dd/yyyy")}`}</span>
-              {opendate && <DateRange
-                editableDateInputs={true}
-                onChange={item => setdate([item.selection])}
-                moveRangeOnFirstSelection={false}
-                ranges={date}
-                className="date"
-              />}
-            </div>
+              <div className="header-search-content">
+                <FontAwesomeIcon icon={faCalendarDay} className="header-icon" />
+                <span onClick={() => setopendate(!opendate)} className="headerSearchText">{`${format(date[0].startDate, "MM/dd/yyyy")} to ${format(date[0].endDate, "MM/dd/yyyy")}`}</span>
+                {opendate && <DateRange
+                  editableDateInputs={true}
+                  onChange={item => setdate([item.selection])}
+                  moveRangeOnFirstSelection={false}
+                  ranges={date}
+                  className="date"
+                />}
+              </div>
 
-            <div className="header-search-content">
-              <FontAwesomeIcon icon={'calendar-day'} className="header-icon" />
-              <span onClick={() => { setdaysoption(!daysoption) }} className="headerSearchText">{`${days.day} days ${days.time} time ${days.delay} delay`}</span>
-              {daysoption && <div className="delivery-time">
-                <div className="optionItem">
-                  <span className="optionText">Days</span>
-                  <div className="optionCounter">
-                    <button disabled={days.day <= 1} className="optionCounterButton " onClick={() => (handleOption("day", "d"))}>-</button>
-                    <div className="optionCounterNumber">{days.day}</div>
-                    <button className="optionCounterButton" onClick={() => (handleOption("day", "i"))}>+</button>
+              <div className="header-search-content">
+                <FontAwesomeIcon icon={'calendar-day'} className="header-icon" />
+                <span onClick={() => { setdaysoption(!daysoption) }} className="headerSearchText">{`${days.day} days ${days.time} time ${days.delay} delay`}</span>
+                {daysoption && <div className="delivery-time">
+                  <div className="optionItem">
+                    <span className="optionText">Days</span>
+                    <div className="optionCounter">
+                      <button disabled={days.day <= 1} className="optionCounterButton " onClick={() => (handleOption("day", "d"))}>-</button>
+                      <div className="optionCounterNumber">{days.day}</div>
+                      <button className="optionCounterButton" onClick={() => (handleOption("day", "i"))}>+</button>
+                    </div>
                   </div>
-                </div>
 
 
-                <div className="optionItem">
-                  <span className="optionText">Time</span>
-                  <div className="optionCounter">
-                    <button disabled={days.time <= 1} className="optionCounterButton" onClick={() => (handleOption("time", "d"))}>-</button>
-                    <div className="optionCounterNumber">{days.time}</div>
-                    <button className="optionCounterButton" onClick={() => (handleOption("time", "i"))}>+</button>
+                  <div className="optionItem">
+                    <span className="optionText">Time</span>
+                    <div className="optionCounter">
+                      <button disabled={days.time <= 1} className="optionCounterButton" onClick={() => (handleOption("time", "d"))}>-</button>
+                      <div className="optionCounterNumber">{days.time}</div>
+                      <button className="optionCounterButton" onClick={() => (handleOption("time", "i"))}>+</button>
+                    </div>
                   </div>
-                </div>
-                <div className="optionItem">
-                  <span className="optionText">Delay</span>
-                  <div className="optionCounter">
-                    <button disabled={days.delay <= 0} className="optionCounterButton" onClick={() => (handleOption("delay", "d"))}>-</button>
-                    <div className="optionCounterNumber">{days.delay}</div>
-                    <button className="optionCounterButton" onClick={() => (handleOption("delay", "i"))}>+</button>
+                  <div className="optionItem">
+                    <span className="optionText">Delay</span>
+                    <div className="optionCounter">
+                      <button disabled={days.delay <= 0} className="optionCounterButton" onClick={() => (handleOption("delay", "d"))}>-</button>
+                      <div className="optionCounterNumber">{days.delay}</div>
+                      <button className="optionCounterButton" onClick={() => (handleOption("delay", "i"))}>+</button>
+                    </div>
                   </div>
-                </div>
-              </div>}
+                </div>}
+
+              </div>
+              <div className="header-search-content">
+                <button className="headerBtn" onClick={handleSearch}>
+                  Search
+                </button>
+              </div>
 
             </div>
-
-
-          </div>
           </>}
 
       </div>
