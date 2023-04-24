0af92c1829c3b7ff8e526180ad7c863800d0bebc
Shashank Tiwari <shashanktiwari3509@gmail.com>
2023-04-25 00:00:04 +0530

    list added.

--- client/src/pages/List.jsx
+++ client/src/pages/List.jsx
@@ -3,6 +3,7 @@ import { useLocation } from 'react-router-dom'
 import { Header } from '../components/Header'
 import { Navbar } from '../components/Navbar'
 import { useState } from 'react'
+import { format } from 'date-fns'
 
 const List = () => {
   const location = useLocation();
@@ -17,11 +18,11 @@ const List = () => {
             <h1 className="lsTitle">Search</h1>
             <div className="lsItem">
               <label>Delivery</label>
-              <input type="text" className=""/>
+              <input placeholder={delivery} type="text" className=""/>
             </div>
             <div className="lsItem">
-              <label>Delivery</label>
-              <input type="text" className=""/>
+              <label>Departure-date</label>
+              <span>{`${format(date[0].startDate, "MM/dd/yyyy")} to ${format(date[0].endDate, "MM/dd/yyyy")}`}</span>
             </div>
           </div>
           <div className="listResult"></div>
