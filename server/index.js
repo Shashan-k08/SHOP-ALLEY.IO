a742716e008627fb02fd958fea94d5b77d5866ac
Shashank Tiwari <shashanktiwari3509@gmail.com>
2024-01-02 23:55:18 +0530

    MongoDb connected

--- server/index.js
+++ server/index.js
@@ -1,5 +1,27 @@
-const express = require('express');
-
+const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 
-const router = require('router');
+//const router = require('router');
+const connect = async () => {
+  try {
+    await mongoose.connect("mongodb://localhost:27017/eMart");
+    console.log("Connected to db");
+  } catch (err) {
+    throw err;
+  }
+};
+mongoose.connection.on("disconnected", () => {
+  console.log("Mongodb Disconnected");
+});
+mongoose.connection.on("connected", () => {
+  console.log("mongoDb connected");
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello connection");
+});
+app.listen(5000, () => {
+  connect();
+  console.log("Connected to backend!");
+});
