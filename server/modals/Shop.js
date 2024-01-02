ac93ab3c5410f3728101c1d7759d9a95e36d3e01
Shashank Tiwari <shashanktiwari3509@gmail.com>
2024-01-03 01:12:24 +0530

    Modals added and Shop schema created

---
+++ server/modals/Shop.js
@@ -0,0 +1,49 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const ShopSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+
+  type: {
+    type: String,
+    required: true,
+  },
+  city: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  distance: {
+    type: String,
+    required: true,
+  },
+  images: {
+    type: [String],
+  },
+  desc: {
+    type: String,
+    required: true,
+  },
+  rating: {
+    type: Number,
+    min: 0,
+    max: 5,
+  },
+  items: {
+    type: [String],
+  },
+  cheapestItemPrice: {
+    type: Number,
+    required: true,
+  },
+  featured: {
+    type: Boolean,
+    required: true,
+  },
+});
